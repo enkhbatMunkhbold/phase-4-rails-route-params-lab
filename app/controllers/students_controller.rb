1ff513895dfba219746d339c297c4af42e83e860
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2023-03-30 10:58:01 -0700

    commit tree 7a073eae446f3f750c6bba259f82b07fd4f79b51

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,16 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = Student.all.each{|s| s.first_name.downcase}
+
+    students.find_by(first_name: params[:name])
+    byebug
     render json: students
+  end
+
+  def student_info
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
