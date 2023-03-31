4975edc44398968a5cb47b1b20969cdb97b977fb
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2023-03-30 23:18:44 -0700

    Completed Assignment with Bonus part

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,19 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    if params[:name]
+      name = params[:name].capitalize
+      students = Student.all.filter{|s| s.first_name == name || s.last_name == name}
+      # students = Student.all.where(first_name: name).or(Student.all.where(last_name: name))
+    else
+      students = Student.all
+    end
     render json: students
+  end
+
+  def student_info
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
